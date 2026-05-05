76b301783a2a9692e94b86d368e08afa2576bf0e
syaofox <syaofox@gmail.com>
2026-05-05 12:02:48 +0800

    numlock / caplock 显示大写字母

--- config.def.h
+++ config.def.h
@@ -93,7 +93,7 @@ static const struct arg args[] = {
 	{ separator,     		"%s",         				" | ",     			0,			"sep" },
 	{ swap_perc,     		"SWAP %s%%", 				NULL, 				2000,		"swap" },
 	{ separator,     		"%s",         				" | ",     			0,			"sep" },
-	{ keyboard_indicators, 	"%s", 						"c?n?", 			0,			"kbd" },
+	{ keyboard_indicators, 	"%s", 						"C?N?", 			0,			"kbd" },
 	{ separator,     		"%s",         				" | ",     			0,			"sep" },
 	{ datetime,    			"%s", 						"%m-%d %H:%M", 		5000,		"time" },
 	{ separator,     		"%s",         				" | ",     			0,			"sep" },
