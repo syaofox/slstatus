9d97c6e81682bf25a57f0b75729373abd074de37
syaofox <syaofox@gmail.com>
2026-05-05 11:43:38 +0800

    feat: cpu 温度

--- config.def.h
+++ config.def.h
@@ -78,6 +78,7 @@ static const struct arg args[] = {
 	{ netspeed_auto, 		" %s",      					NULL,               1000,		"net" },
 	{ separator,     		"%s",         				" | ",     			0,			"sep" },
 	{ cpu_perc,     		"CPU %s%% ", 				NULL, 				2000,		"cpu" },
+	{ temp,         		"%s°C ", 					"/sys/class/thermal/thermal_zone0/temp", 2000,	"temp" },
 	{ ram_used,    			"RAM %s", 					"1000", 			2000,		"ram" },
 	{ separator,     		"%s",         				" | ",     			0,			"sep" },
 	{ gpu_combined, 		"%s", 						NULL, 				3000,		"gpu" },
